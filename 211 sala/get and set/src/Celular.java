a702451628a26306ee68b1be4194ed9499dcba01
julianoADeretti <juliano_deretti@estudante.sesisenai.org.br>
2025-10-28 21:13:27 -0300

    Add files via upload

---
+++ 211 sala/get and set/src/Celular.java
@@ -0,0 +1,19 @@
+public class Celular {
+
+    private String marca;
+    private String sistema;
+    private String cor;
+    private String modelo;
+
+    public Celular(String marca, String sistema, String cor, String modelo) {
+        this.marca = marca;
+        this.sistema = sistema;
+        this.cor = cor;
+        this.modelo = modelo;
+
+
+
+
+        
+
+}
