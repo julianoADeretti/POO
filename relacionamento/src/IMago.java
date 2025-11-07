9b21989eb744dd3ffabea52087c645334f41203d
julianoADeretti <juliano_deretti@estudante.sesisenai.org.br>
2025-11-06 21:41:37 -0300

    Update IMago.java

--- relacionamento/src/IMago.java
+++ relacionamento/src/IMago.java
@@ -1,34 +1,23 @@
 public interface IMago {
-
-
     float getVida();
     void setVida(float vida);
-
 
     boolean isGrimorio();
     void setGrimorio(boolean grimorio);
 
-
     boolean isAcessorio();
     void setAcessorio(boolean acessorio);
 
-
     float getMana();
+
     void setMana(float mana);
-
-
     String getMagia();
-    void setMagia(String magia);
 
+    void setMagia(String magia);
 
     int getFeiticos();
     void setFeiticos(int feiticos);
 
-
     float getForca();
     void setForca(float forca);
-
-
 }
-
-
